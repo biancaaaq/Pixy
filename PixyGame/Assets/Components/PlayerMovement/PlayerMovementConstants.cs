c2106edf41e9b77a6d20bce0d0336202d2bdbf89
andreimoise1 <moisefacultate1@gmail.com>
2025-11-03 22:08:39 +0200

    fix: base game

---
+++ PixyGame/Assets/Components/PlayerMovement/PlayerMovementConstants.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+public static class PlayerMovementConstants
+{
+    public static readonly float moveSpeed = 5f;
+    public static readonly float jumpForce = 10f;
+    public static readonly float groundCheckRadius = 0.2f;
+    public static readonly float gravityScale = 3f;
+}
--- PixyGame/Assets/constants/LoadingScreen.constant.cs
+++ PixyGame/Assets/constants/LoadingScreen.constant.cs
@@ -1,9 +1,10 @@
-using System.Numerics;
 using UnityEngine;
 public static class LoadingScreenConstant
 {
     public static readonly int length = 100;
-    public static readonly int startingHeight = 0;
+    public static readonly int horizontalStart = 5;
     public static readonly int maximumHeight = 5;
     public static readonly int maximumStep = 1;
+    public static readonly int startHeight = -20;
+    public static readonly int startWidth = -20;
 }
