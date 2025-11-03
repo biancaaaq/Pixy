c321338f5c8cd9e906b71f23e6c2b92ca8b0a843
andreimoise1 <moisefacultate1@gmail.com>
2025-11-03 17:42:28 +0200

    feat: partial loading screen

---
+++ PixyGame/Assets/constants/LoadingScreen.constant.cs
@@ -0,0 +1,9 @@
+using System.Numerics;
+using UnityEngine;
+public static class LoadingScreenConstant
+{
+    public static readonly int length = 100;
+    public static readonly int startingHeight = 0;
+    public static readonly int maximumHeight = 5;
+    public static readonly int maximumStep = 1;
+}
